e6632eb50bc49ba3d93e13ebf7ee29b5b2f6f0ed
Arianny21 <95243008+Arianny21@users.noreply.github.com>
2021-12-10 20:58:19 -0400

    Add files via upload

---
+++ BDlogin/BDlogin/Form5.Designer.cs
@@ -0,0 +1,311 @@
+﻿
+namespace BDlogin
+{
+    partial class FormProductos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormProductos));
+            this.button5 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textCantDispo = new System.Windows.Forms.TextBox();
+            this.textPrecio = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textCategoria = new System.Windows.Forms.TextBox();
+            this.textMarca = new System.Windows.Forms.TextBox();
+            this.textNombreProducto = new System.Windows.Forms.TextBox();
+            this.textIdProducto = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.button6 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(852, 513);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(194, 37);
+            this.button5.TabIndex = 70;
+            this.button5.Text = "CERRAR SESION";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(12, 350);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(220, 20);
+            this.label6.TabIndex = 69;
+            this.label6.Text = "CANTIDAD DISPONIBLE";
+            // 
+            // textCantDispo
+            // 
+            this.textCantDispo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textCantDispo.Location = new System.Drawing.Point(16, 373);
+            this.textCantDispo.Name = "textCantDispo";
+            this.textCantDispo.Size = new System.Drawing.Size(216, 27);
+            this.textCantDispo.TabIndex = 68;
+            // 
+            // textPrecio
+            // 
+            this.textPrecio.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textPrecio.Location = new System.Drawing.Point(16, 320);
+            this.textPrecio.Name = "textPrecio";
+            this.textPrecio.Size = new System.Drawing.Size(216, 27);
+            this.textPrecio.TabIndex = 67;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 297);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(78, 20);
+            this.label5.TabIndex = 66;
+            this.label5.Text = "PRECIO";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 235);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(115, 20);
+            this.label4.TabIndex = 65;
+            this.label4.Text = "CATEGORIA";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 173);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 20);
+            this.label3.TabIndex = 64;
+            this.label3.Text = "MARCA";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(199, 20);
+            this.label2.TabIndex = 63;
+            this.label2.Text = "NOMBRE PRODUCTO";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 20);
+            this.label1.TabIndex = 62;
+            this.label1.Text = "ID PRODUCTO";
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(915, 433);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(131, 37);
+            this.button4.TabIndex = 61;
+            this.button4.Text = "NUEVO";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(718, 433);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(130, 37);
+            this.button3.TabIndex = 60;
+            this.button3.Text = "ELIMINAR";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(479, 433);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(156, 37);
+            this.button2.TabIndex = 59;
+            this.button2.Text = "MODIFICAR";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(267, 433);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 37);
+            this.button1.TabIndex = 58;
+            this.button1.Text = "INSERTAR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textCategoria
+            // 
+            this.textCategoria.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textCategoria.Location = new System.Drawing.Point(16, 258);
+            this.textCategoria.Name = "textCategoria";
+            this.textCategoria.Size = new System.Drawing.Size(216, 27);
+            this.textCategoria.TabIndex = 57;
+            // 
+            // textMarca
+            // 
+            this.textMarca.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textMarca.Location = new System.Drawing.Point(16, 196);
+            this.textMarca.Name = "textMarca";
+            this.textMarca.Size = new System.Drawing.Size(216, 27);
+            this.textMarca.TabIndex = 56;
+            // 
+            // textNombreProducto
+            // 
+            this.textNombreProducto.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textNombreProducto.Location = new System.Drawing.Point(16, 143);
+            this.textNombreProducto.Name = "textNombreProducto";
+            this.textNombreProducto.Size = new System.Drawing.Size(216, 27);
+            this.textNombreProducto.TabIndex = 55;
+            // 
+            // textIdProducto
+            // 
+            this.textIdProducto.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textIdProducto.Location = new System.Drawing.Point(16, 81);
+            this.textIdProducto.Name = "textIdProducto";
+            this.textIdProducto.Size = new System.Drawing.Size(134, 27);
+            this.textIdProducto.TabIndex = 54;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dataGridView2);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(267, 40);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(779, 366);
+            this.groupBox1.TabIndex = 53;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(0, 18);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(773, 342);
+            this.dataGridView2.TabIndex = 0;
+            this.dataGridView2.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(20, 513);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(130, 37);
+            this.button6.TabIndex = 71;
+            this.button6.Text = "MENU";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // FormProductos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.ClientSize = new System.Drawing.Size(1058, 571);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textCantDispo);
+            this.Controls.Add(this.textPrecio);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textCategoria);
+            this.Controls.Add(this.textMarca);
+            this.Controls.Add(this.textNombreProducto);
+            this.Controls.Add(this.textIdProducto);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FormProductos";
+            this.Text = "PRODUCTOS";
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textCantDispo;
+        private System.Windows.Forms.TextBox textPrecio;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textCategoria;
+        private System.Windows.Forms.TextBox textMarca;
+        private System.Windows.Forms.TextBox textNombreProducto;
+        private System.Windows.Forms.TextBox textIdProducto;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button button6;
+    }
+}
---
+++ BDlogin/BDlogin/Form5.cs
@@ -0,0 +1,140 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BDlogin
+{
+    public partial class FormProductos : Form
+    {
+        public FormProductos()
+        {
+            InitializeComponent();
+        }
+
+        static string conexion = "SERVER=127.0.0.1; PORT=3306; DATABASE=bdlogin1; UID=root; PASSWORD=;";
+        MySqlConnection cn = new MySqlConnection(conexion);
+
+        public DataTable llenar_grid()
+        {
+            cn.Open();
+            DataTable dt = new DataTable();
+            String llenar = "Select * from productos";
+            MySqlCommand cmd = new MySqlCommand(llenar, cn);
+            MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+            da.Fill(dt);
+            cn.Close();
+
+            return dt;
+        }
+
+            private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+            {
+                try
+                {
+                    textIdProducto.Text = dataGridView2.CurrentRow.Cells[0].Value.ToString();
+                    textNombreProducto.Text = dataGridView2.CurrentRow.Cells[1].Value.ToString();
+                    textMarca.Text = dataGridView2.CurrentRow.Cells[2].Value.ToString();
+                    textCategoria.Text = dataGridView2.CurrentRow.Cells[3].Value.ToString();
+                    textPrecio.Text = dataGridView2.CurrentRow.Cells[4].Value.ToString();
+                    textCantDispo.Text = dataGridView2.CurrentRow.Cells[5].Value.ToString();
+                }
+                catch
+                {
+
+                }
+            }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            cn.Open();
+
+            String insertar = "INSERT INTO productos(IDPRODUCTO, NOMBREPRODUCTO, MARCA, CATEGORIA, PRECIO, CANTDISPO)values(@idProducto, @nombreProducto, @marca, @categoria, @precio, @cantDispo)";
+            MySqlCommand cmd = new MySqlCommand(insertar, cn);
+
+            cmd.Parameters.AddWithValue("@idProducto", textIdProducto.Text);
+            cmd.Parameters.AddWithValue("@nombreProducto", textNombreProducto.Text);
+            cmd.Parameters.AddWithValue("@marca", textMarca.Text);
+            cmd.Parameters.AddWithValue("@categoria", textCategoria.Text);
+            cmd.Parameters.AddWithValue("@precio", textPrecio.Text);
+            cmd.Parameters.AddWithValue("@cantDispo", textCantDispo.Text);
+
+            cmd.ExecuteNonQuery();
+
+            cn.Close();
+            MessageBox.Show("Los datos fueron agregados exitosamente.");
+
+
+            dataGridView2.DataSource = llenar_grid();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            cn.Open();
+            string modificar = "UPDATE productos SET IDPRODUCTO=@idProducto, NOMBREPRODUCTO=@nombreProducto, MARCA=@marca, CATEGORIA=@categoria, PRECIO=@precio, CANTDISPO=@cantDispo WHERE IDPRODUCTO=@idProducto";
+
+            MySqlCommand cmd = new MySqlCommand(modificar, cn);
+
+            cmd.Parameters.AddWithValue("@idProducto", textIdProducto.Text);
+            cmd.Parameters.AddWithValue("@nombreProducto", textNombreProducto.Text);
+            cmd.Parameters.AddWithValue("@marca", textMarca.Text);
+            cmd.Parameters.AddWithValue("@categoria", textCategoria.Text);
+            cmd.Parameters.AddWithValue("@precio", textPrecio.Text);
+            cmd.Parameters.AddWithValue("@cantDispo", textCantDispo.Text);
+
+            cmd.ExecuteNonQuery();
+
+            cn.Close();
+            MessageBox.Show("Datos modificados correctamente");
+
+            dataGridView2.DataSource = llenar_grid();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            cn.Open();
+
+            String eliminar = "DELETE FROM productos WHERE IDPRODUCTO=@idProducto";
+
+            MySqlCommand cmd = new MySqlCommand(eliminar, cn);
+            cmd.Parameters.AddWithValue("@idProducto", textIdProducto.Text);
+            cmd.ExecuteNonQuery();
+            cn.Close();
+
+            MessageBox.Show("Los datos fueron borrados exitosamente.");
+
+            dataGridView2.DataSource = llenar_grid();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textIdProducto.Clear();
+            textNombreProducto.Clear();
+            textMarca.Clear();
+            textCategoria.Clear();
+            textPrecio.Clear();
+            textCantDispo.Clear();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Form formulario1 = new FormLogin();
+            formulario1.Show();
+            this.Hide();
+        }
+
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            FormPrincipal menu = new FormPrincipal();
+            menu.Show();
+            this.Hide();
+        }
+    }
+    }
